d3d09e7ce6972732a14c561e75a5b6d975c7d2fb
Impxct <44038762+Impxct@users.noreply.github.com>
2022-02-25 14:44:20 -0700

    Add files via upload

---
+++ Moonbase Alpha/FormMain.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Moonbase_Alpha
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxAreaInformation = new System.Windows.Forms.GroupBox();
+            this.textBoxAreaDescription = new System.Windows.Forms.TextBox();
+            this.textBoxAreaName = new System.Windows.Forms.TextBox();
+            this.labelDesription = new System.Windows.Forms.Label();
+            this.labelName = new System.Windows.Forms.Label();
+            this.groupBoxNav = new System.Windows.Forms.GroupBox();
+            this.buttonEast = new System.Windows.Forms.Button();
+            this.buttonWest = new System.Windows.Forms.Button();
+            this.buttonSouth = new System.Windows.Forms.Button();
+            this.buttonNorth = new System.Windows.Forms.Button();
+            this.groupBoxAreaInformation.SuspendLayout();
+            this.groupBoxNav.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBoxAreaInformation
+            // 
+            this.groupBoxAreaInformation.Controls.Add(this.textBoxAreaDescription);
+            this.groupBoxAreaInformation.Controls.Add(this.textBoxAreaName);
+            this.groupBoxAreaInformation.Controls.Add(this.labelDesription);
+            this.groupBoxAreaInformation.Controls.Add(this.labelName);
+            this.groupBoxAreaInformation.Location = new System.Drawing.Point(738, 12);
+            this.groupBoxAreaInformation.Name = "groupBoxAreaInformation";
+            this.groupBoxAreaInformation.Size = new System.Drawing.Size(343, 353);
+            this.groupBoxAreaInformation.TabIndex = 0;
+            this.groupBoxAreaInformation.TabStop = false;
+            this.groupBoxAreaInformation.Text = "Area Information";
+            // 
+            // textBoxAreaDescription
+            // 
+            this.textBoxAreaDescription.Location = new System.Drawing.Point(6, 149);
+            this.textBoxAreaDescription.Multiline = true;
+            this.textBoxAreaDescription.Name = "textBoxAreaDescription";
+            this.textBoxAreaDescription.Size = new System.Drawing.Size(263, 134);
+            this.textBoxAreaDescription.TabIndex = 4;
+            // 
+            // textBoxAreaName
+            // 
+            this.textBoxAreaName.Location = new System.Drawing.Point(6, 44);
+            this.textBoxAreaName.Name = "textBoxAreaName";
+            this.textBoxAreaName.ReadOnly = true;
+            this.textBoxAreaName.Size = new System.Drawing.Size(263, 20);
+            this.textBoxAreaName.TabIndex = 3;
+            // 
+            // labelDesription
+            // 
+            this.labelDesription.AutoSize = true;
+            this.labelDesription.Location = new System.Drawing.Point(6, 133);
+            this.labelDesription.Name = "labelDesription";
+            this.labelDesription.Size = new System.Drawing.Size(85, 13);
+            this.labelDesription.TabIndex = 2;
+            this.labelDesription.Text = "Area Description";
+            this.labelDesription.Click += new System.EventHandler(this.labelDesription_Click);
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(6, 28);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(60, 13);
+            this.labelName.TabIndex = 1;
+            this.labelName.Text = "Area Name";
+            // 
+            // groupBoxNav
+            // 
+            this.groupBoxNav.Controls.Add(this.buttonEast);
+            this.groupBoxNav.Controls.Add(this.buttonWest);
+            this.groupBoxNav.Controls.Add(this.buttonSouth);
+            this.groupBoxNav.Controls.Add(this.buttonNorth);
+            this.groupBoxNav.Location = new System.Drawing.Point(747, 371);
+            this.groupBoxNav.Name = "groupBoxNav";
+            this.groupBoxNav.Size = new System.Drawing.Size(315, 211);
+            this.groupBoxNav.TabIndex = 1;
+            this.groupBoxNav.TabStop = false;
+            this.groupBoxNav.Text = "Navigation System";
+            // 
+            // buttonEast
+            // 
+            this.buttonEast.Location = new System.Drawing.Point(225, 59);
+            this.buttonEast.Name = "buttonEast";
+            this.buttonEast.Size = new System.Drawing.Size(75, 71);
+            this.buttonEast.TabIndex = 3;
+            this.buttonEast.Text = "East";
+            this.buttonEast.UseVisualStyleBackColor = true;
+            this.buttonEast.Click += new System.EventHandler(this.buttonEast_Click);
+            // 
+            // buttonWest
+            // 
+            this.buttonWest.Location = new System.Drawing.Point(6, 59);
+            this.buttonWest.Name = "buttonWest";
+            this.buttonWest.Size = new System.Drawing.Size(75, 71);
+            this.buttonWest.TabIndex = 2;
+            this.buttonWest.Text = "West";
+            this.buttonWest.UseVisualStyleBackColor = true;
+            this.buttonWest.Click += new System.EventHandler(this.buttonWest_Click);
+            // 
+            // buttonSouth
+            // 
+            this.buttonSouth.Location = new System.Drawing.Point(114, 141);
+            this.buttonSouth.Name = "buttonSouth";
+            this.buttonSouth.Size = new System.Drawing.Size(86, 54);
+            this.buttonSouth.TabIndex = 1;
+            this.buttonSouth.Text = "South";
+            this.buttonSouth.UseVisualStyleBackColor = true;
+            this.buttonSouth.Click += new System.EventHandler(this.buttonSouth_Click);
+            // 
+            // buttonNorth
+            // 
+            this.buttonNorth.Location = new System.Drawing.Point(114, 19);
+            this.buttonNorth.Name = "buttonNorth";
+            this.buttonNorth.Size = new System.Drawing.Size(86, 55);
+            this.buttonNorth.TabIndex = 0;
+            this.buttonNorth.Text = "North";
+            this.buttonNorth.UseVisualStyleBackColor = true;
+            this.buttonNorth.Click += new System.EventHandler(this.buttonNorth_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Moonbase_Alpha.Properties.Resources.moonbasept2;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1093, 594);
+            this.Controls.Add(this.groupBoxNav);
+            this.Controls.Add(this.groupBoxAreaInformation);
+            this.Name = "FormMain";
+            this.Text = "Moonbase Alpha";
+            this.groupBoxAreaInformation.ResumeLayout(false);
+            this.groupBoxAreaInformation.PerformLayout();
+            this.groupBoxNav.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxAreaInformation;
+        private System.Windows.Forms.Label labelDesription;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.TextBox textBoxAreaDescription;
+        private System.Windows.Forms.TextBox textBoxAreaName;
+        private System.Windows.Forms.GroupBox groupBoxNav;
+        private System.Windows.Forms.Button buttonEast;
+        private System.Windows.Forms.Button buttonWest;
+        private System.Windows.Forms.Button buttonSouth;
+        private System.Windows.Forms.Button buttonNorth;
+    }
+}
---
+++ Moonbase Alpha/FormMain.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Moonbase_Alpha
+{
+    public partial class FormMain : Form
+
+    // runs when form loads 1 time
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+            // sets up name of room that is started in.
+            textBoxAreaName.Text = "General Purpose Room";
+            // sets up the description of the room started in.
+            textBoxAreaDescription.Text = "You are in the General Purpose Room. This room has a little of everything in it.";
+        }
+        /// <summary>
+        /// The code below handles the event of user clicking North
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+
+
+        private void labelDesription_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void buttonNorth_Click(object sender, EventArgs e)
+        {
+            //this event is raised when the user clicks north button from the gpr
+            //changes area name
+            textBoxAreaName.Text = "North Landing Pad";
+            //changes area description
+            textBoxAreaDescription.Text = "Eagle transporter on the North Landing Pad.";
+            //changes the area background
+            this.BackgroundImage = Properties.Resources.LandingPad;
+        }
+
+        private void buttonWest_Click(object sender, EventArgs e)
+        {
+            //this event is raised when the user clicks West button from the gpr
+            //changes area name
+            textBoxAreaName.Text = "West Wing Comms Room.";
+            //changes area description
+            textBoxAreaDescription.Text = "This is the communications room.";
+            //changes the area background
+            this.BackgroundImage = Properties.Resources.CommunicationRoom;
+        }
+
+        private void buttonSouth_Click(object sender, EventArgs e)
+        {
+            //this event is raised when the user clicks South button from the gpr
+            //changes area name
+            textBoxAreaName.Text = "Soutern Corridors";
+            //changes area description
+            textBoxAreaDescription.Text = "Here are the main Corridors. Decide where you will go.";
+            //changes the area background
+            this.BackgroundImage = Properties.Resources.SouthCorridors_;
+        }
+
+        private void buttonEast_Click(object sender, EventArgs e)
+        {
+            //this event is raised when the user clicks east button from the gpr
+            //changes area name
+            textBoxAreaName.Text = "Eastern Living Quarters.";
+            //changes area description
+            textBoxAreaDescription.Text = "This is your room! Get as comforatable as you'd like.";
+            //changes the area background
+            this.BackgroundImage = Properties.Resources.usersroom;
+        }
+    }
+}
