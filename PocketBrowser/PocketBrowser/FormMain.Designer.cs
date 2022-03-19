056d9b63967493718ac0b3df0e0da7f3d45e5b8c
Impxct <44038762+Impxct@users.noreply.github.com>
2022-03-19 16:02:40 -0700

    Add files via upload

---
+++ PocketBrowser/PocketBrowser/FormMain.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace PocketBrowser
+{
+    partial class MainBrowser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GoButton = new System.Windows.Forms.Button();
+            this.textBoxURL = new System.Windows.Forms.TextBox();
+            this.webBrowserDisplay = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // GoButton
+            // 
+            this.GoButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GoButton.Location = new System.Drawing.Point(12, 0);
+            this.GoButton.Name = "GoButton";
+            this.GoButton.Size = new System.Drawing.Size(134, 94);
+            this.GoButton.TabIndex = 0;
+            this.GoButton.Text = "Go";
+            this.GoButton.UseVisualStyleBackColor = true;
+            this.GoButton.Click += new System.EventHandler(this.GoButton_Click);
+            // 
+            // textBoxURL
+            // 
+            this.textBoxURL.Location = new System.Drawing.Point(152, 39);
+            this.textBoxURL.Name = "textBoxURL";
+            this.textBoxURL.Size = new System.Drawing.Size(1672, 20);
+            this.textBoxURL.TabIndex = 1;
+            // 
+            // webBrowserDisplay
+            // 
+            this.webBrowserDisplay.Location = new System.Drawing.Point(12, 100);
+            this.webBrowserDisplay.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowserDisplay.Name = "webBrowserDisplay";
+            this.webBrowserDisplay.Size = new System.Drawing.Size(1812, 520);
+            this.webBrowserDisplay.TabIndex = 2;
+            // 
+            // MainBrowser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::PocketBrowser.Properties.Resources.space_pulsar_homepage_;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1836, 632);
+            this.Controls.Add(this.webBrowserDisplay);
+            this.Controls.Add(this.textBoxURL);
+            this.Controls.Add(this.GoButton);
+            this.Name = "MainBrowser";
+            this.Text = "Browser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button GoButton;
+        private System.Windows.Forms.TextBox textBoxURL;
+        private System.Windows.Forms.WebBrowser webBrowserDisplay;
+    }
+}
+
---
+++ PocketBrowser/PocketBrowser/FormMain.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PocketBrowser
+{
+    public partial class MainBrowser : Form
+    {
+        /// <summary>
+        /// Runs once the program starts, Runs automatically. 
+        /// </summary>
+        public MainBrowser()
+        {
+            InitializeComponent();
+            //default home page.
+            textBoxURL.Text = "google.com";
+            //Goes to the URL typed into URL textbox. Displays web page in the browser.
+            webBrowserDisplay.Navigate(textBoxURL.Text);
+            // Stops javascript errors from displaying
+            webBrowserDisplay.ScriptErrorsSuppressed = true;
+
+        }
+        /// <summary>
+        ///  Runs when user clicks "Go" Button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GoButton_Click(object sender, EventArgs e)
+        {
+            //Goes to the URL typed into URL textbox. Displays web page in the browser.
+            webBrowserDisplay.Navigate(textBoxURL.Text);
+            // Stops javascript errors from displaying
+            webBrowserDisplay.ScriptErrorsSuppressed = true;
+            //pasted above for auto display of "google.com"
+        }
+    }
+}
